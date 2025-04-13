deea8cf68ae38e16ba65e797b1eb93f93422d56c
helmutsirait77 <helmutsirait77@gmail.com>
2025-04-13 19:57:28 +0700

    struktur kendali switch

--- script.js
+++ script.js
@@ -45,12 +45,20 @@
 // }
 
 // switch 
-var angka = 4;
+var item = prompt('masukkan nama makanan / minuman : \n (cth: nasi, daging, susu, hamburger, softdrink)');
 
-if( angka === 1 ) {
-	 alert('anda memasukkan angka 1');
-}else if( angka === 2 ) {
-	  alert('anda memasukkan angka 2');
-}else {
-	 alert('angka yang anda masukkan salah');
+switch ( item ) {
+	  case 'nasi':
+     case 'daging' :
+     case 'susu' : 
+        alert('makanan / minuman SEHAT');
+        break;
+      case 'hamburger' :
+      case 'softdrink' :
+        alert('makanan / minuman TIDAK SEHAT');
+        break;       		
+	   default:
+		 alert('anda memasukkan nama makanan / minuman yang salah'); 
+		break;
 }
+
