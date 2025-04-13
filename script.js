c1fc0a6a48afcce5e92d1507305440742fa89f20
helmutsirait77 <helmutsirait77@gmail.com>
2025-04-13 19:28:09 +0700

    switch

---
+++ script.js
@@ -0,0 +1,56 @@
+// var nama = prompt('masukkan nama');
+// alert(nama);
+
+// var test = confirm('Kamu Yakin??');
+// if( test === true ) {
+//    alert('User Menekan tombol ok');
+// } else {
+// 	 alert('User menekan tombol cancel');
+// }
+
+// alert('Selamat lagi');
+// var lagi = true;
+
+// while( lagi ) {
+//      var nama = prompt('Masukkan Nama');
+//      alert('halo '+ nama);
+
+//      lagi = confirm('coba lagi?');
+// }
+// alert('Terima kasih');
+// var ulang = true;
+// while( ulang ) {
+//       console.log('Hello World');
+//       ulang = confirm('lagi?');
+// }
+
+// var nilaiAwal = 1;
+// while( nilaiAwal <= 10 ) {
+//       console.log('Hello World '+ nilaiAwal +'x');
+//     nilaiAwal++;  
+// }
+
+// for
+// for( var nilaiAwal = 1; nilaiAwal <= 10; nilaiAwal++ ) {
+// 	 console.log('Hello World '+ nilaiAwal +' x');
+// }
+
+// var angka = prompt('Masukkan angka');
+// if( angka % 2 == 0 ) {
+// 	alert(angka + ' adalah bilangan genap' );
+// } else if( angka % 2 == 1 ){
+// 	alert(angka + ' adalah bilangan ganjil' );	 
+// } else {
+// 	 alert('yang anda masukkan bukan angka');
+// }
+
+// switch 
+var angka = 4;
+
+if( angka === 1 ) {
+	 alert('anda memasukkan angka 1');
+}else if( angka === 2 ) {
+	  alert('anda memasukkan angka 2');
+}else {
+	 alert('angka yang anda masukkan salah');
+}
