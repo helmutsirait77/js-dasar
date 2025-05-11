2c268e16d51449ce429c1bda41193073e30dba15
helmutsirait77 <helmutsirait77@gmail.com>
2025-05-11 08:43:26 +0700

    Object

--- function/penumpang/script.js
+++ function/penumpang/script.js
@@ -28,7 +28,9 @@
 
 // var penumpang = [];
 // var penumpang = ['dani', undefined, 'sandi'];
-var penumpang = ['dani', undefined, 'sandi'];
+// var penumpang = ['dani', undefined, 'sandi'];
+var penumpang = [];
+
 // console.log(penumpang.length);
 var tambahPenumpang = function (namaPenumpang, penumpang) {
         // jika angkot kosong
@@ -62,3 +64,48 @@ var tambahPenumpang = function (namaPenumpang, penumpang) {
            }           
        }        
 }
+
+// hapus penumpang 
+// 2 parameter 
+  // - nama Penumpang
+  // - array Penumpang 
+// Rules 
+  /*
+      - jika angkot kosong, tampilkan pesan angkot kosong 
+     - jika penumpang yang namanya sesuai, hapus nama penumpang pada array 
+       dengan memberi nilai undefined 
+     - jika tidak ada penumpang yang namanya sesuai, tampilkan pesan kesalahannya 
+  */  
+
+  var hapusPenumpang = function(namaPenumpang, penumpang) {
+           if(penumpang.length == 0) {
+               console.log('Angkot Masih Kosong');
+               return penumpang;
+           } else {
+                 for(var i = 0; i < penumpang.length; i++) {
+                      if(penumpang[i] == namaPenumpang) {
+                           penumpang[i] = undefined;
+                           return penumpang;
+                      } else if( penumpang[i] !== namaPenumpang) {
+                           console.log(namaPenumpang+' sudah ada di dalam angkot');
+                           return penumpang;
+                      }
+                 }
+           }
+  } 
+
+
+  /*
+     - jika angkot kosong 
+       - tampilkan pesan bahwa angkot kosong, dan 
+         tidak mungkin ada penumpang yang turun 
+       - kembalikan isi array & keluar dari function 
+     - else 
+        - telusuri kursi dari awal 
+           - jika nama penumpang sesuai 
+            - hapus penumpang dengan mengubah namanya menjadi undefined 
+            - kembalikan isi array & keluar dari function 
+          - jika tidak ada nama yang sesuai 
+             - tampilkan pesan kesalahanya 
+             - kembalikan isi array & keluar dari function        
+  */ 
---
+++ object/script.js
@@ -0,0 +1,11 @@
+// membuat object 
+var mhs = {
+	nama: 'Dani',
+    umur: 31,
+    ips: [3.00, 2.50, 3.20],
+    alamat: {
+    	 jalan: 'Jln, abc No. 123',
+    	 kota: 'Medan',
+    	 provinsi: 'Sumatera Utara'
+    }
+};
